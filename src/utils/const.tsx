2490ab402271a527b2eb4441928b2c332e56f3ed
Istudiohtml <istudiohtml@gmail.com>
2022-10-18 22:29:51 +0700

    change endpoint

--- src/utils/const.tsx
+++ src/utils/const.tsx
@@ -1,3 +1,3 @@
 
-export const API_PATH = process.env.NEXT_PUBLIC_APP_ENV === 'production' ? 'http://127.0.0.1:8000/api' : 'http://cornea-analysis.com/api';
+export const API_PATH = process.env.NEXT_PUBLIC_APP_ENV === 'localhost' ? 'http://127.0.0.1:8000/api' : 'http://cornea-analysis.com/api';
 
