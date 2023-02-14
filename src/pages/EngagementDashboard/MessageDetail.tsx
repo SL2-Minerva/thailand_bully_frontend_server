99409a308b0ba96008ec386f9f332380cbc3f3d9
Spacetrax <spacetrax@Spacetraxs-MacBook-Pro.local>
2023-02-14 17:49:46 +0700

    sent params for select period for sentiment score and engagement rate

--- src/pages/EngagementDashboard/MessageDetail.tsx
+++ src/pages/EngagementDashboard/MessageDetail.tsx
@@ -80,7 +80,7 @@ const MessageDetail = (props: DialogInfoProps) => {
       end_date_period: params?.previousEndDate ? moment(params?.previousEndDate).format('YYYY-MM-DD') : '',
       page: page,
       limit: 10,
-      report_nubmer: reportNo,
+      report_number: reportNo,
       page_name: params?.page,
       label: params?.label
     }
@@ -96,7 +96,7 @@ const MessageDetail = (props: DialogInfoProps) => {
       organization_id: paramsId?.organization_id || '',
       page: page,
       limit: 10,
-      report_nubmer: reportNo,
+      report_number: reportNo,
       page_name: params?.page,
       label: params?.label
     }
